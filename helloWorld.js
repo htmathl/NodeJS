b46e50d0931c2ea8c632e71804b05a9067c13616
MATHEUS FERREIRA DOMINGUES <matheus.fdomingues1@senacsp.edu.br>
2024-08-31 08:19:08 -0300

    First Commit

---
+++ helloWorld.js
@@ -0,0 +1,13 @@
+//string
+console.log('Hello World!');
+console.log('5'+'2');
+
+//int
+console.log(5%2);
+
+//bool
+console.log(true);
+
+const idade = 27
+
+console.log(`A sua idade é: ${idade >= 28? idade+5 : idade-5}`);
